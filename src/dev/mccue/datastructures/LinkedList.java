0b03914b0b42503e2e3bae23b8ed0958474a16f2
Ethan McCue <ethan@mccue.dev>
2022-06-01 20:25:16 -0400

    ...

--- src/dev/mccue/datastructures/LinkedList.java
+++ src/dev/mccue/datastructures/LinkedList.java
@@ -1,8 +1,17 @@
 package dev.mccue.datastructures;
 
+/**
+ * "Sum Type" representation of a linked list.
+ */
 public sealed interface LinkedList<T> {
+    /**
+     * An empty list.
+     */
     record Empty<T>() 
         implements LinkedList<T> {}
+    /**
+     * A not empty list.
+     */
     record NotEmpty<T>(T first, LinkedList<T> rest) 
         implements LinkedList<T> {}
 }
