28386b583231ebd8253c5ee186bf6a2b16444ae7
Ethan McCue <ethan@mccue.dev>
2022-06-01 20:19:25 -0400

    ...

--- src/dev/mccue/datastructures/LinkedList.java
+++ src/dev/mccue/datastructures/LinkedList.java
@@ -3,6 +3,6 @@ package dev.mccue.datastructures;
 public sealed interface LinkedList<T> {
     record Empty<T>() 
         implements LinkedList<T> {}
-    record NotEmpty(T first, LinkedList<T> rest) 
+    record NotEmpty<T>(T first, LinkedList<T> rest) 
         implements LinkedList<T> {}
 }
